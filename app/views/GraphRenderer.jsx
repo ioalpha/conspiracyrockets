9736fc1614b5ebf7441a228d386fb3578aef9a74
Leon Weidauer <leon@lnwdr.de>
2015-08-22 20:02:09 +0200

    whitespace

--- app/views/GraphRenderer.jsx
+++ app/views/GraphRenderer.jsx
@@ -1,147 +1,147 @@
 GraphRenderer = (function(React) {
     return React.createClass({
 
-    dummyObject :   {
-	    "name": "Top Level",
-	    "parent": "null",
-	    "children": [
-	      {
-	        "name": "Level 2: A",
-	        "parent": "Top Level",
-	        "children": [
-	          {
-	            "name": "Son of A",
-	            "parent": "Level 2: A",
-	            "children":[]
-	          },
-	          {
-	            "name": "Daughter of A",
-	            "parent": "Level 2: A",
-	            "children":[]
-	          }
-	        ]
-	      },
-	      {
-	        "name": "Level 2: B",
-	        "parent": "Top Level",
-	        "children":[]
-	      }
-	    ]
-	  },
+        dummyObject :   {
+            "name": "Top Level",
+            "parent": "null",
+            "children": [
+                {
+                "name": "Level 2: A",
+                "parent": "Top Level",
+                "children": [
+                    {
+                    "name": "Son of A",
+                    "parent": "Level 2: A",
+                    "children":[]
+                },
+                {
+                    "name": "Daughter of A",
+                    "parent": "Level 2: A",
+                    "children":[]
+                }
+                ]
+            },
+            {
+                "name": "Level 2: B",
+                "parent": "Top Level",
+                "children":[]
+            }
+            ]
+        },
 
-	  find: function(haystack, needle) {
-	  if (haystack.name === needle) return haystack;
-	  for (var i = 0; i < haystack.children.length; i ++) {
-	    var result = this.find(haystack.children[i], needle);
-	    if (result) return result;
-	  }
-	  return null;
-	},
+        find: function(haystack, needle) {
+            if (haystack.name === needle) return haystack;
+            for (var i = 0; i < haystack.children.length; i ++) {
+                var result = this.find(haystack.children[i], needle);
+                if (result) return result;
+            }
+            return null;
+        },
 
-      create: function(state){
-      	// tbd
-      	var el = this.getDOMNode();
+        create: function(state){
+            // tbd
+            var el = this.getDOMNode();
 
-      	this.svg = d3.select(this.getDOMNode()).append("svg")
-			.attr("width","100%")
-			.attr("height",el.clientHeight+"px")
-			.append("g")
-			.attr("width","200px")
-			.attr("height","500px")
-			.attr("transform", "translate(100,0)");
+            this.svg = d3.select(this.getDOMNode()).append("svg")
+            .attr("width","100%")
+            .attr("height",el.clientHeight+"px")
+            .append("g")
+            .attr("width","200px")
+            .attr("height","500px")
+            .attr("transform", "translate(100,0)");
 
-  		this.update(state);
-      },
+            this.update(state);
+        },
 
-      update: function(state){
+        update: function(state){
 
-		var el = this.getDOMNode();
-		var self = this;
+            var el = this.getDOMNode();
+            var self = this;
 
-		 var tree = d3.layout.tree()
-			.size([200, 500]);
+            var tree = d3.layout.tree()
+            .size([200, 500]);
 
-		var diagonal = d3.svg.diagonal()
-			.projection(function(d) { return [d.y, d.x]; });
-      	
-      	// prepare data
-      	var untouchedData = state.data;
-      	var treeData = _.clone(untouchedData, true);
+            var diagonal = d3.svg.diagonal()
+            .projection(function(d) { return [d.y, d.x]; });
 
-      	//remove old graph
-      	d3.selectAll('g > *').remove();
+            // prepare data
+            var untouchedData = state.data;
+            var treeData = _.clone(untouchedData, true);
 
-		var nodes = tree.nodes(treeData).reverse();
-		var links = tree.links(nodes);
+            //remove old graph
+            d3.selectAll('g > *').remove();
 
-		  // Declare the nodes…
-		var node = this.svg.selectAll("g.node")
-			  .data(nodes);
+            var nodes = tree.nodes(treeData).reverse();
+            var links = tree.links(nodes);
 
-		  // Enter the nodes.
-		  var nodeEnter = node.enter().append("g")
-			  .attr("class", "node")
-			  .attr("transform", function(d) { 
-				  return "translate(" + d.y + "," + d.x + ")"; });
+            // Declare the nodes…
+            var node = this.svg.selectAll("g.node")
+            .data(nodes);
 
-		  nodeEnter.append("circle")
-			  .attr("r", 10)
-			  .style("fill", "#fff")
-			  .on("click", function(d){
-			  	var new_node = {"name" : "random new node", "children" : []};
-			  	self.find(untouchedData,d.name).children.push(new_node);
-			  	var root_node = _.clone(untouchedData,true);
-			  	self.update({'data':root_node});
-			  });
+            // Enter the nodes.
+            var nodeEnter = node.enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { 
+                return "translate(" + d.y + "," + d.x + ")"; });
 
-			// TODO: hier muss das neue Objekt nach außen gereicht werden
+                nodeEnter.append("circle")
+                .attr("r", 10)
+                .style("fill", "#fff")
+                .on("click", function(d){
+                    var new_node = {"name" : "random new node", "children" : []};
+                    self.find(untouchedData,d.name).children.push(new_node);
+                    var root_node = _.clone(untouchedData,true);
+                    self.update({'data':root_node});
+                });
 
-		  nodeEnter.append("text")
-			  .attr("x", function(d) { 
-				  return d.children || d._children ? -13 : 13; })
-			  .attr("dy", ".35em")
-			  .attr("text-anchor", function(d) { 
-				  return d.children || d._children ? "end" : "start"; })
-			  .text(function(d) { return d.name; })
-			  .style("fill-opacity", 1);
+                // TODO: hier muss das neue Objekt nach außen gereicht werden
 
-	  // Declare the links…
-	  var link = this.svg.selectAll("path.link")
-		  .data(links);
+                nodeEnter.append("text")
+                .attr("x", function(d) { 
+                    return d.children || d._children ? -13 : 13; })
+                    .attr("dy", ".35em")
+                    .attr("text-anchor", function(d) { 
+                        return d.children || d._children ? "end" : "start"; })
+                        .text(function(d) { return d.name; })
+                        .style("fill-opacity", 1);
 
-	  // Enter the links.
-	  link.enter().insert("path", "g")
-		  .attr("class", "link")
-		  .attr("d", diagonal);
+                        // Declare the links…
+                        var link = this.svg.selectAll("path.link")
+                        .data(links);
+
+                        // Enter the links.
+                        link.enter().insert("path", "g")
+                        .attr("class", "link")
+                        .attr("d", diagonal);
 
 
-      },
+        },
 
-	  componentDidMount: function() {
-	    this.create(this.getChartState());
-	  },
+        componentDidMount: function() {
+            this.create(this.getChartState());
+        },
 
-	  componentDidUpdate: function() {
-	    this.update(this.getChartState());
-	  },
+        componentDidUpdate: function() {
+            this.update(this.getChartState());
+        },
 
-	  getChartState: function() {
-	    /*return {
-	      data: this.props.data,
-	    };*/
-	    return {
-	    	data : this.dummyObject
-	    }
-	  },
-
-        render: function() {
-            return (
-                <div className="graphrenderer">
-                	{}
-                </div>
-            );
+        getChartState: function() {
+            /*return {
+data: this.props.data,
+};*/
+        return {
+data : this.dummyObject
         }
-    });
-})(React);
+},
+
+            render: function() {
+                return (
+                    <div className="graphrenderer">
+                        {}
+                    </div>
+                );
+            }
+        });
+    })(React);
 
 
